e88eae1ff3432259d57d509947bdcee9a2ba1790
J Nithin John <134169460+tropicroc@users.noreply.github.com>
2025-04-04 15:00:03 +0530

    Update testfile.py

--- testfile.py
+++ testfile.py
@@ -1,5 +1,5 @@
 import whisper
 
 model = whisper.load_model("tiny")
-result = model.transcribe("test-audio-lol.mp3")
+result = model.transcribe("test-audio-lol.mp3", fp16=False)
 print(result["text"])
